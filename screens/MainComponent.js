b04ab7a1c079df309372bd27b568dc7c04d52565
Liam Goheen <lkgoheen@hotmail.com>
2022-09-02 22:02:18 -0600

    forms

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -7,6 +7,7 @@ import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '
 import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
+import ReservationScreen from './ReservationScreen';
 import { Icon } from 'react-native-elements';
 import logo from '../assets/images/logo.png';
 import { useDispatch } from 'react-redux';
@@ -124,6 +125,29 @@ const ContactNavigator = () => {
     )
 }
 
+const ReservationNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen 
+                name='Reservation'
+                component={ReservationScreen}
+                options={({ navigation }) => ({
+                    title: 'Reservation Search',  
+                    headerLeft: () => (
+                        <Icon 
+                            name='tree'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    )
+}
+
 const CustomDrawerContent = (props) => (
     <DrawerContentScrollView {...props}>
         <View style={styles.drawerHeader}>
@@ -147,7 +171,7 @@ const Main = () => {
         dispatch(fetchPartners());
         dispatch(fetchComments());
     }, [dispatch]);
-    
+
     return (
         <View 
             style={{ 
@@ -185,6 +209,22 @@ const Main = () => {
                         drawerIcon: ({ color }) => (
                             <Icon 
                                 name='list'
+                                type='font-awesome'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                     }}
+                />
+                <Drawer.Screen 
+                    name='ReserveCampsite'
+                    component={ReservationNavigator}
+                    options={{ 
+                        title: 'Reserve Campsite',
+                        drawerIcon: ({ color }) => (
+                            <Icon 
+                                name='tree'
                                 type='font-awesome'
                                 size={24}
                                 iconStyle={{ width: 24 }}
